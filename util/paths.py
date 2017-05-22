50142b7602635827adc4821a9c2042b78a0afbb2
putin <vp@fsb.gov>
2017-05-22 15:43:19 -0700

    t2: folder restructuring

--- util/paths.py
+++ util/paths.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-virtual_mount_point = "./virtual"
+virtual_mount_point = "../virtual"
 vlc_basename = "vlc"
 vadc_name = "vadc"
 logfile_directory = "./logs"
-datalog_directory = "./data_logs"
+datalog_directory = "../data_logs"
 logfile_extenstion = "_log_"
 
